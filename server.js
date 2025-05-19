a7e9ad3bc2182dc45d2c71ea366d7458b52bfb8b
rini001 <debnathrenaissance95@gmail.com>
2025-05-19 18:52:26 +0530

    changes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://bml-client.vercel.app', // frontend URL
+   origin: process.env.CORS_ALLOWED_ORIGIN || 'http://localhost:5173',
   credentials: true,
 }));
 
